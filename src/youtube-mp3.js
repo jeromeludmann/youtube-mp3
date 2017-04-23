f7ea4b7241f1a184e0319767d066bdd82bf5bf92
Jérôme Ludmann <jerome@ludmann.io>
2017-04-23 19:56:55 +0200

    Add multiple downloading

--- src/youtube-mp3.js
+++ src/youtube-mp3.js
@@ -10,133 +10,137 @@ const METADATA_ARGS = {
 }
 
 export default class YoutubeMP3 extends EventEmitter {
-  constructor(options) {
-    super()
-    if (!options.youtubeUrl) {
-      throw new Error('missing attribute: youtubeUrl')
-    }
-    this.options = options
-    this.options.id = url.parse(options.youtubeUrl, true).query.v
+  constructor(params) {
+    super(params)
+    this.params = params
   }
 
   /**
    * Start audio extraction process
    */
-  start() {
-    this.downloadFromYoutube()
+  async start() {
+    for (const param of this.params) {
+      await this.downloadFromYoutube(param)
+    }
   }
 
   /**
    * Extract audio stream from Youtube video
    */
-  downloadFromYoutube() {
-    let extractedFile = null
-    const baseFilename = `${Date.now()}${this.options.id}`
+  downloadFromYoutube(param) {
+    return new Promise((resolve, reject) => {
+      let extractedFile = null
+      const baseFilename = `${Date.now()}_${url.parse(param.youtubeUrl, true).query.v}`
 
-    const youtubeDlArgs = [
-      '--verbose',
-      '--add-metadata',
-      '--extract-audio',
-      // '--prefer-ffmpeg',
-      // '--prefer-avconv',
-      this.options.youtubeUrl,
-      '-o',
-      `${baseFilename}.%(ext)s`
-    ]
+      const youtubeDlArgs = [
+        '--verbose',
+        '--add-metadata',
+        '--extract-audio',
+        // '--prefer-ffmpeg',
+        // '--prefer-avconv',
+        param.youtubeUrl,
+        '-o',
+        `${baseFilename}.%(ext)s`
+      ]
 
-    this.emit('data', JSON.stringify(youtubeDlArgs))
+      this.emit('data', JSON.stringify(youtubeDlArgs))
 
-    const youtubedl = spawn(path.resolve(__dirname, '..', 'bin', 'youtube-dl'), youtubeDlArgs)
+      const youtubedl = spawn(path.resolve(__dirname, '..', 'bin', 'youtube-dl'), youtubeDlArgs)
 
-    const onData = data => {
-      const filter = '[ffmpeg] Adding metadata to'
-      const line = data.toString()
+      const onData = data => {
+        const filter = '[ffmpeg] Adding metadata to'
+        const line = data.toString()
 
-      if (line.indexOf(filter) > -1) {
-        extractedFile = line.slice(filter.length + 2, line.length - 2).trim()
+        if (line.indexOf(filter) > -1) {
+          extractedFile = line.slice(filter.length + 2, line.length - 2).trim()
+        }
+
+        this.emit('data', line)
       }
 
-      this.emit('data', line)
-    }
+      const onClose = code => {
+        resolve(this.convertToMp3(param, extractedFile, baseFilename))
+      }
 
-    const onClose = code => {
-      this.convertToMp3(extractedFile, baseFilename)
-    }
-
-    youtubedl.stdout.on('data', onData)
-    youtubedl.stderr.on('data', onData)
-    youtubedl.on('close', onClose)
+      youtubedl.stdout.on('data', onData)
+      youtubedl.stderr.on('data', onData)
+      youtubedl.on('close', onClose)
+    })
   }
 
   /**
    * Convert the given source file to MP3
    */
-  convertToMp3(src, dest) {
-    const ffmpegArgs = [
-      '-loglevel', 'verbose',
-      '-i', src,
-      '-vn',
-      '-sn',
-      '-y',
-      '-c:a', 'mp3'
-    ]
+  convertToMp3(param, src, dest) {
+    return new Promise((resolve, reject) => {
+      const ffmpegArgs = [
+        '-loglevel', 'verbose',
+        '-i', src,
+        '-vn',
+        '-sn',
+        '-y',
+        '-c:a', 'mp3'
+      ]
 
-    const extension = src.slice(src.lastIndexOf('.') + 1)
-    const metadataArg = METADATA_ARGS[extension]
-    if (!metadataArg) {
-      throw new Error(`file extension ${extension} not found in metadata args`)
-    }
-    ffmpegArgs.push('-map_metadata', metadataArg)
+      const extension = src.slice(src.lastIndexOf('.') + 1)
+      const metadataArg = METADATA_ARGS[extension]
+      if (!metadataArg) {
+        throw new Error(`file extension ${extension} not found in metadata args`)
+      }
+      ffmpegArgs.push('-map_metadata', metadataArg)
 
-    if (this.options.tags) {
-      ffmpegArgs.push('-metadata', `title=${this.options.tags.title}`)
-      ffmpegArgs.push('-metadata', `artist=${this.options.tags.artist}`)
-      ffmpegArgs.push('-metadata', `album=${this.options.tags.album}`)
-    }
+      if (param.tags) {
+        ffmpegArgs.push('-metadata', `title=${param.tags.title}`)
+        ffmpegArgs.push('-metadata', `artist=${param.tags.artist}`)
+        ffmpegArgs.push('-metadata', `album=${param.tags.album}`)
+      }
 
-    ffmpegArgs.push('-b:a', this.options.quality
-      ? this.options.quality
-      : '320k')
+      ffmpegArgs.push('-b:a', param.quality
+        ? param.quality
+        : '320k')
 
-    if (this.options.start) {
-      ffmpegArgs.push('-ss', this.options.start)
-    }
+      if (param.start) {
+        ffmpegArgs.push('-ss', param.start)
+      }
 
-    if (this.options.duration) {
-      ffmpegArgs.push('-t', this.options.duration)
-    }
+      if (param.duration) {
+        ffmpegArgs.push('-t', param.duration)
+      }
 
-    const filename = `${dest}.mp3`
-    ffmpegArgs.push(filename)
+      const filename = `${dest}.mp3`
+      ffmpegArgs.push(filename)
 
-    this.emit('data', JSON.stringify(ffmpegArgs))
+      this.emit('data', JSON.stringify(ffmpegArgs))
 
-    const ffmpeg = spawn('ffmpeg', ffmpegArgs)
+      const ffmpeg = spawn('ffmpeg', ffmpegArgs)
 
-    const onData = data => {
-      const line = data.toString()
-      this.emit('data', line)
-    }
+      const onData = data => {
+        const line = data.toString()
+        this.emit('data', line)
+      }
 
-    const onClose = code => {
-      fs.unlink(src, err => {
-        if (err) {
-          this.emit('error', `Error while removing temporary file: ${src}`)
-        } else {
-          this.emit('data', `Temporary file removed: ${src}`)
-        }
+      const onClose = code => {
+        fs.unlink(src, err => {
+          if (err) {
+            this.emit('error', `Error while removing temporary file: ${src}`)
+          } else {
+            this.emit('data', `Temporary file removed: ${src}`)
+          }
 
-        if (code !== 0) {
-          this.emit('error', filename, code)
-          return
-        }
+          if (code !== 0) {
+            this.emit('error', filename, code)
+            reject(new Error(code))
+            return
+          }
 
-        this.emit('success', filename)
-      })
-    }
+          this.emit('success', filename)
+          resolve(filename)
+        })
+      }
 
-    ffmpeg.stdout.on('data', onData)
-    ffmpeg.stderr.on('data', onData)
-    ffmpeg.on('close', onClose)
+      ffmpeg.stdout.on('data', onData)
+      ffmpeg.stderr.on('data', onData)
+      ffmpeg.on('close', onClose)
+    })
   }
 }
